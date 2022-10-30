f1082fec168e7f778febbfc6eb3644ea34aad1a8
CrappyDill <mddillon47@gmail.com>
2022-10-29 18:25:21 -0700

    card

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="bg-felt h-screen w-screen">
-    
+  <div class="bg-felt h-screen w-screen p-10">
+        <img :src="cards.cards[0].image" />
   </div>
 </template>
+
+<script setup>
+
+const { data: cards } = await useFetch('https://www.deckofcardsapi.com/api/deck/new/draw/?count=2')
+</script>
