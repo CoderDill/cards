641478f2209707fed5157e5aa65a96bfa7f50210
CrappyDill <mddillon47@gmail.com>
2022-10-30 00:27:19 -0700

    dealer/player cards

--- app.vue
+++ app.vue
@@ -1,10 +1,36 @@
 <template>
   <div class="bg-felt h-screen w-screen p-10">
-        <img :src="cards.cards[0].image" />
+    <div class="flex justify-around">
+      <h1>Dealer</h1>
+      <h1>Player</h1>
+    </div>
+    <div class="">
+      
+        <div class="flex">
+          <img :src="cards.cards[0].image" />
+          <img :src="cards.cards[1].image" />
+        </div>
+        <div class="flex">
+          <img :src="cards.cards[2].image" />
+          <img :src="cards.cards[3].image" />
+        </div>
+      
+    </div>
   </div>
 </template>
 
 <script setup>
+const { data: newDeck } = await useFetch(
+  "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2"
+);
+const deckId = ref(newDeck.value.deck_id);
+const { data: cards } = await useFetch(
+  `https://www.deckofcardsapi.com/api/deck/${deckId.value}/draw/?count=4`
+);
 
-const { data: cards } = await useFetch('https://www.deckofcardsapi.com/api/deck/new/draw/?count=2')
+async function drawCard() {
+  const { data: drawCard } = await useFetch(
+    `https://www.deckofcardsapi.com/api/deck/${deckId.value}/draw/?count=1`
+  );
+}
 </script>
